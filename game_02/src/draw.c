720ed7f981ff1c20c517fd42232ccafc102a75d9
cristhianjhl.com <cristhianjhl.web@gmail.com>
2025-01-05 16:18:14 -0500

    update player texture size

--- game_02/src/draw.c
+++ game_02/src/draw.c
@@ -18,6 +18,8 @@ void blit(SDL_Texture *texture, int x, int y) {
     SDL_Rect dest;
     dest.x = x;
     dest.y = y;
+    dest.w = 100;
+    dest.h = 50;
     SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
     SDL_RenderCopy(app.renderer, texture, NULL, &dest);
 }
