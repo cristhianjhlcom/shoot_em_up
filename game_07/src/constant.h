fd8c06f8b367a087d4abd74510fbe266440991e6
cristhianjhl.com <cristhianjhl.web@gmail.com>
2025-01-10 20:57:53 -0500

    add bullet collision with enemies then destroy it

---
+++ game_07/src/constant.h
@@ -0,0 +1,13 @@
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+#define PLAYER_WIDTH 151
+#define PLAYER_HEIGHT 101
+#define PLAYER_SPEED 4
+#define PLAYER_BULLET_SPEED 5
+#define MAX_KEYBOARD_KEYS 350
+
+#define SIDE_PLAYER 0
+#define SIDE_ALIEN 1
