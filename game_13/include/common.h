2dc5c1bf381e071d780e0ef146e8ef59e09b6130
Cristhian <cristhianjhl.web@gmail.com>
2025-02-09 10:24:23 -0500

    add highscore table exercise

---
+++ game_13/include/common.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_events.h>
+#include <SDL_image.h>
+#include <SDL_log.h>
+#include <SDL_mixer.h>
+#include <SDL_rect.h>
+#include <SDL_scancode.h>
+#include <SDL_timer.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "constant.h"
+#include "draw.h"
+#include "input.h"
+#include "music.h"
+#include "setup.h"
+#include "stage.h"
+#include "text.h"
+#include "structs.h"
+#include "util.h"
